f03d3a0ed91da5128ecd365be6682ee3dd252fec
Vineet Sharma <vineetmacbook@Vineets-MacBook-Air.local>
2024-11-27 14:40:03 +0530

    Operations in js

---
+++ 01_Basics/04_operations.js
@@ -0,0 +1,30 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Vineet"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+// this is a very bad way to write the code. one of the most imp. thing in coding is "code readability"
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
